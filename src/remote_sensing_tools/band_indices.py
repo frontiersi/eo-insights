235bc21ce8f317b1f89041cfe398db76ebf7914e
Madeleine Seehaber <mseehaber@frontiersi.com.au>
2024-05-10 09:04:47 +1000

    works returning the specified indices, however returns ds with all bands and indices

--- src/remote_sensing_tools/band_indices.py
+++ src/remote_sensing_tools/band_indices.py
@@ -1,21 +1,42 @@
-def calculate_indices(data):
+def calculate_indices(ds, index):
 
-    # calculate indicies
-    # Modified Soil Adjusted Vegetation Index (MSAVI)
-    msavi = (
-        2 * data.nir + 1 - ((2 * data.nir + 1) ** 2 - 8 * (data.nir - data.red)) ** 0.5
-    ) / 2
-    data["msavi"] = msavi
-    # Built-up Area Extraction Index (BAEI)
-    baei = (data.red + 0.3) / (data.green + data.swir_1)
-    data["baei"] = baei
-    # Bare Soil Index (BSI)
-    bsi = ((data.swir_1 + data.red) - (data.nir + data.blue)) / (
-        (data.swir_1 + data.red) + (data.nir + data.blue)
-    )
-    data["bsi"] = bsi
-    # Normalised Difference Water Index (NDWI)
-    ndwi = (data.green - data.nir) / (data.green + data.nir)
-    data["ndwi"] = ndwi
+    # Dictionary containing remote sensing index band indices
+    index_dict = {
+        # Modified Soil Adjusted Vegetation Index (MSAVI)
+        "msavi": lambda ds: (
+            2 * ds.nir + 1 - ((2 * ds.nir + 1) ** 2 - 8 * (ds.nir - ds.red)) ** 0.5
+        )
+        / 2,
+        # data["msavi"] = msavi
+        # Built-up Area Extraction Index (BAEI)
+        "baei": lambda ds: (ds.red + 0.3) / (ds.green + ds.swir_1),
+        # data["baei"] = baei
+        # Bare Soil Index (BSI)
+        "bsi": lambda ds: ((ds.swir_1 + ds.red) - (ds.nir + ds.blue))
+        / ((ds.swir_1 + ds.red) + (ds.nir + ds.blue)),
+        # data["bsi"] = bsi
+        # Normalised Difference Water Index (NDWI)
+        "ndwi": lambda ds: (ds.green - ds.nir) / (ds.green + ds.nir),
+        # data["ndwi"] = ndwi}
+    }
 
-    return data
+    # If index supplied is not a list, convert to list. This allows us to
+    # iterate through either multiple or single indices in the loop below
+    indices = index if isinstance(index, list) else [index]
+
+    # Calculate for each index in the list of indices supplied (indexes)
+    for index in indices:
+
+        # Select an index function from the dictionary
+        index_func = index_dict.get(str(index))
+
+        if index is None:
+            raise ValueError(f"No remote sensing `index` was provided.")
+
+        mult = 1.0
+        index_array = index_func(ds / mult)
+
+        output_band_name = index
+        ds[output_band_name] = index_array
+
+    return ds
