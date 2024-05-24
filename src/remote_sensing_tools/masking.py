ac0e679337d87a0f2da022bf2bf39a8d1dc7abeb
Caitlin Adams <caitlin.isabel.adams@gmail.com>
2024-05-24 15:57:50 +1000

    Add functionality for generating categorical mask. Add functionality to apply mask to data

--- src/remote_sensing_tools/masking.py
+++ src/remote_sensing_tools/masking.py
@@ -1,18 +1,21 @@
 """Functionality for managing data masks"""
 
-from typing import Union, Optional
+import logging
+from typing import Union
 import xarray
 from remote_sensing_tools.stac_utils import MaskInfo
 
 XarrayType = Union[xarray.Dataset, xarray.DataArray]
 
+_log = logging.getLogger(__name__)
+
 
 def set_mask_attributes(
-    mask: xarray.DataArray, mask_info: Optional[MaskInfo]
+    mask: xarray.DataArray, mask_info: MaskInfo
 ) -> xarray.DataArray:
     """Attach information in MaskInfo to xarray.DataArray attribute"""
 
-    if (mask_info is not None) and (mask.name == mask_info.alias):
+    if mask.name == mask_info.alias:
         mask.attrs.update(
             collection=mask_info.collection,
             mask_type=mask_info.mask_type,
@@ -20,7 +23,23 @@ def set_mask_attributes(
             flags_definition=mask_info.flags_definition,
         )
     else:
-        # TODO: Replace with log statement
-        print("mask did not match with mask info")
+        raise ValueError(
+            f"Mask ({mask.name}) did not match MaskInfo ({mask_info.alias}). No attributes were added to {mask.name}"
+        )
 
     return mask
+
+
+def generate_categorical_mask(
+    mask: xarray.DataArray, categories: list[str], category_values: dict[str, int]
+) -> xarray.DataArray:
+    """Generate a categorical mask"""
+
+    values = [category_values[category] for category in categories]
+
+    mask_bool = mask.isin(values)
+
+    mask_bool.attrs = mask.attrs
+    mask_bool.attrs.update(mask_type="boolean")
+
+    return mask_bool
--- src/remote_sensing_tools/raster_base.py
+++ src/remote_sensing_tools/raster_base.py
@@ -9,7 +9,7 @@ import odc.stac
 import pystac_client
 import xarray
 from remote_sensing_tools.stac_utils import STACConfig
-from remote_sensing_tools.masking import set_mask_attributes
+from remote_sensing_tools.masking import set_mask_attributes, generate_categorical_mask
 
 # Construct types for type hinting
 BBox = tuple[float, float, float, float]
@@ -124,3 +124,47 @@ class RasterBase:
             _log.info("No masks were found for the requested collections")
 
         return cls(data, masks)
+
+    def apply_mask(self, mask_name):
+        """For a given mask, create the relevant boolean mask and apply to data"""
+
+        try:
+            mask = self.masks[mask_name]
+        except KeyError as e:
+            raise KeyError(
+                f"Mask '{mask_name}' was not recognised. Available masks are {list(self.masks.data_vars)}"
+            ) from e
+
+        # Determine the mask type from the mask attributes
+        mask_type = mask.attrs.get("mask_type")
+
+        if mask_type is not None:
+            if mask_type == "categorical":
+                mask_categories = mask.attrs.get("categories_to_mask")
+                mask_category_values = mask.attrs.get("flags_definition").get("values")
+
+                _log.info(
+                    "Selecting all pixels belonging to any of %s", mask_categories
+                )
+                self.masks[mask_name] = generate_categorical_mask(
+                    mask=mask,
+                    categories=mask_categories,
+                    category_values=mask_category_values,
+                )
+            else:
+                raise NotImplementedError(
+                    f"No mask generation functionality exists for masks of type {mask_type}. Valid mask types are ['categorical']"
+                )
+        else:
+            raise ValueError(
+                "No mask type was found. Ensure the mask has a valid 'mask_type' attribute."
+            )
+
+        # Invert the boolean mask before applying it to the data
+        inverted_mask = ~self.masks[mask_name]
+
+        # Apply the mask to each variable
+        for variable in self.data:
+            self.data[variable] = self.data[variable].where(
+                inverted_mask, other=self.data[variable].odc.nodata
+            )
