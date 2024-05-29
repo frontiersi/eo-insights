cb3b23a5bc1a2f0179b45d11949a5a73a72fe91c
Caitlin Adams <caitlin.isabel.adams@gmail.com>
2024-05-29 17:05:59 +1000

    Implement ability to seperately generate boolean masks from stored masks

--- src/remote_sensing_tools/raster_base.py
+++ src/remote_sensing_tools/raster_base.py
@@ -125,46 +125,114 @@ class RasterBase:
 
         return cls(data, masks)
 
-    def apply_mask(self, mask_name):
-        """For a given mask, create the relevant boolean mask and apply to data"""
+    def generate_boolean_mask(self, mask_name: str, inplace: bool = True):
+        """For a given mask, create the relevant boolean mask"""
 
-        try:
-            mask = self.masks[mask_name]
-        except KeyError as e:
-            raise KeyError(
-                f"Mask '{mask_name}' was not recognised. Available masks are {list(self.masks.data_vars)}"
-            ) from e
+        if self.masks is not None:
+            try:
+                # Ignoring type error because str is both hashable and iterable of hashable
+                # https://github.com/pydata/xarray/blob/cd3ab8d5580eeb3639d38e1e884d2d9838ef6aa1/xarray/core/dataset.py#L1552
+                mask: xarray.DataArray = self.masks[mask_name]  # type: ignore
+            except KeyError as e:
+                raise KeyError(
+                    f"Mask '{mask_name}' was not recognised. Available masks are {list(self.masks.data_vars)}"
+                ) from e
+        else:
+            raise ValueError(
+                f"The mask attribute for this {type(self)} is None. There is no mask to be applied."
+            )
 
         # Determine the mask type from the mask attributes
         mask_type = mask.attrs.get("mask_type")
 
+        # If the user is doing this inplace, the boolean mask will overwrite the original mask
+        if inplace:
+            destination = mask_name
+        else:
+            destination = f"{mask_name}_bool"
+
         if mask_type is not None:
             if mask_type == "categorical":
                 mask_categories = mask.attrs.get("categories_to_mask")
-                mask_category_values = mask.attrs.get("flags_definition").get("values")
+                if mask_categories is None:
+                    raise ValueError(
+                        f"{mask_name} has no categories to mask. Check metadata for categories to mask."
+                    )
+                mask_flags_definition = mask.attrs.get("flags_definition")
+                if mask_flags_definition is not None:
+                    mask_category_values = mask_flags_definition.get("values")
+                else:
+                    raise ValueError(
+                        f"{mask_name} has no flag definitions. Check metadata for mask."
+                    )
 
                 _log.info(
                     "Selecting all pixels belonging to any of %s", mask_categories
                 )
-                self.masks[mask_name] = generate_categorical_mask(
+
+                self.masks[destination] = generate_categorical_mask(
                     mask=mask,
                     categories=mask_categories,
                     category_values=mask_category_values,
                 )
+            elif mask_type == "boolean":
+                _log.info("Using boolean mask as is.")
             else:
                 raise NotImplementedError(
-                    f"No mask generation functionality exists for masks of type {mask_type}. Valid mask types are ['categorical']"
+                    f"{mask_name} has mask type {mask_type}, but no generation strategies exist for this type. Valid mask types are ['categorical', 'boolean']"
                 )
         else:
             raise ValueError(
-                "No mask type was found. Ensure the mask has a valid 'mask_type' attribute."
+                "No mask type was found. Ensure {mask_name} has a valid 'mask_type' attribute."
             )
 
-        # Invert the boolean mask before applying it to the data
-        inverted_mask = ~self.masks[mask_name]
+    def apply_mask(
+        self, mask_name: str, data_inplace: bool = True, mask_inplace: bool = True
+    ):
+        """
+        For a given mask, if it's a boolean, apply to data. If not, create the boolean mask
+        If data_inplace=True, the masked variables will overwrite the unmasked variables.
+        If data_inplace=False, the masked variables wiil be assigned to {variable}_masked.
+        """
+        if self.data is None:
+            raise ValueError(
+                "The data attribute for this {type(self)} is None. There is no data to apply the mask to."
+            )
+
+        if self.masks is not None:
+            try:
+                # Ignoring type error because str is both hashable and iterable of hashable
+                # https://github.com/pydata/xarray/blob/cd3ab8d5580eeb3639d38e1e884d2d9838ef6aa1/xarray/core/dataset.py#L1552
+                mask = self.masks[mask_name]  # type: ignore
+            except KeyError as e:
+                raise KeyError(
+                    f"Mask '{mask_name}' was not recognised. Available masks are {list(self.masks.data_vars)}"
+                ) from e
+        else:
+            raise ValueError(
+                f"The mask attribute for this {type(self)} is None. There is no mask to be applied."
+            )
+
+        # If not dealing with a boolean mask, create the boolean version
+        if mask.attrs.get("mask_type") != "boolean":
+            self.generate_boolean_mask(mask_name, inplace=mask_inplace)
+            if mask_inplace is False:
+                mask = self.masks[f"{mask_name}_bool"]
+
+        # Invert the mask, which is True for bad values, False for good values by design
+        # Inverting allows us to apply it and keep the good values.
+        inverted_mask = ~mask
+
+        # Identify unmasked variables -- this is to ward against mutliple re-runs with inplace=False
+        unmasked_vars = [var for var in list(self.data.keys()) if "_masked" not in var]
 
         # Apply the mask to each variable
-        for variable in self.data:
-            self.data[variable] = self.data[variable].where(
+        for variable in unmasked_vars:
+            if data_inplace:
+                destination = variable
+            else:
+                destination = f"{variable}_masked"
+
+            self.data[destination] = self.data[variable].where(
                 inverted_mask, other=self.data[variable].odc.nodata
             )
